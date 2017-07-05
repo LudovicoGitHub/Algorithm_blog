7a740d39211f166eb6f037a3b4f8a4f4a0ca264a
LudoBessi <ludovicobessi@gmail.com>
2017-07-05 17:08:33 +0200

    Commiting binary search

---
+++ Binary_search.py
@@ -0,0 +1,16 @@
+def binary_search(array):
+    low = 0
+    high = len(array) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        guess = array[mid]
+        if guess == item:
+            return mid
+
+        if guess > item:
+            high = mid - 1
+
+        else:
+            low = mid + 1
+    return None
